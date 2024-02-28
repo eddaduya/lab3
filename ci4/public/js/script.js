31aa96dcbbf4ef669bd71a7a0e09f9b003b1426d
eddaduya <eddaduya@student.apc.edu.ph>
2024-02-28 17:22:04 +0800

    updated html, css, and js

--- ci4/public/js/script.js
+++ ci4/public/js/script.js
@@ -1,18 +1,30 @@
+let experiences = [
+    "#html", 
+    "#css", 
+    "#javascript", 
+    "#python", 
+    "#java", 
+    "#php"
+];
+
 let currentDisplay = 0;
 
-function goLeft(){
+let button1 = document.querySelector("#left-btn")
+button1.addEventListener("click", () => {
     if (currentDisplay == 0){
         currentDisplay = 5
     } else{
     currentDisplay -= 1;
     }
-    
-}
+    button1.setAttribute("href", experiences[currentDisplay])
+})
 
-function goRight(){
+let button2 = document.querySelector("#right-btn")
+button2.addEventListener("click", () => {
     if (currentDisplay == 5){
         currentDisplay = 0
     } else{
     currentDisplay += 1;
     }
-}
+    button2.setAttribute("href", experiences[currentDisplay])
+})
